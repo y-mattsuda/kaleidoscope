61c45c4dec38cdc0ba89b39849d7c3db5eee5f07
y-mattsuda <m.youichi1225@icloud.com>
2023-09-10 23:33:25 +0900

    refactor: if let Some()を使って書き換えた

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdin, Read, Write};
+use std::io::{self, Write};
 
 fn main() {
     let stdin = io::stdin();
@@ -36,35 +36,34 @@ fn gettok(input: &str) -> Vec<Token> {
     let mut input_itr = input.chars();
     let mut next_input = || -> Option<char> { input_itr.next() };
     'main: loop {
-        let _c = next_input();
-        if _c.is_none() {
-            break;
-        }
-        let c = _c.unwrap();
-        if c.is_whitespace() {
-            continue;
-        }
-        // ident: [a-zA-Z][a-zA-Z0-9]*
-        if c.is_alphabetic() {
-            let mut ident_chars: Vec<char> = vec![c];
-            loop {
-                let _next_c = next_input();
-                if _next_c.is_none() {
-                    break 'main;
-                }
-                let next_c = _next_c.unwrap();
-                if next_c.is_alphanumeric() {
-                    ident_chars.push(next_c);
-                } else {
-                    let ident_str: String = ident_chars.into_iter().collect();
-                    match &*ident_str {
-                        "def" => tokens.push(Token::Def),
-                        "extern" => tokens.push(Token::Extern),
-                        _ => tokens.push(Token::Ident(ident_str)),
+        if let Some(c) = next_input() {
+            if c.is_whitespace() {
+                continue;
+            }
+            // ident: [a-zA-Z][a-zA-Z0-9]*
+            if c.is_alphabetic() {
+                let mut ident_chars: Vec<char> = vec![c];
+                'ident_loop: loop {
+                    if let Some(next_c) = next_input() {
+                        if next_c.is_alphanumeric() {
+                            ident_chars.push(next_c);
+                            continue 'ident_loop;
+                        } else {
+                            let ident_str: String = ident_chars.into_iter().collect();
+                            match &*ident_str {
+                                "def" => tokens.push(Token::Def),
+                                "extern" => tokens.push(Token::Extern),
+                                _ => tokens.push(Token::Ident(ident_str)),
+                            }
+                            break 'ident_loop;
+                        }
+                    } else {
+                        break 'main;
                     }
-                    break;
                 }
             }
+        } else {
+            break;
         }
     }
     tokens
