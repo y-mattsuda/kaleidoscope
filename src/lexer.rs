c6733137b2bb9a72fb2720ec6c97eaf7438aadf9
y-mattsuda <m.youichi1225@icloud.com>
2023-09-21 23:00:07 +0900

    refactor: peekableをうまく使うようにした

---
+++ src/lexer.rs
@@ -0,0 +1,149 @@
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    WhiteSpace,
+    Comment(String),
+    // commands
+    Def,
+    Extern,
+    // primary
+    Ident(String),
+    Number(f64),
+}
+
+pub struct Lexer<'a> {
+    // 読み込み中の先頭文字列
+    chars: std::iter::Peekable<std::str::Chars<'a>>,
+}
+#[derive(Debug)]
+pub struct LexerError {
+    pub msg: String,
+}
+
+impl LexerError {
+    fn new(msg: &str) -> Self {
+        LexerError { msg: msg.into() }
+    }
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Lexer {
+            chars: input.chars().peekable(),
+        }
+    }
+    pub fn tokenize(&mut self) -> Result<Vec<Token>, LexerError> {
+        let mut tokens = Vec::<Token>::new();
+        while let Some(token) = self.next_token()? {
+            match token {
+                Token::WhiteSpace => {}
+                Token::Comment(_) => {}
+                _ => tokens.push(token),
+            }
+        }
+        Ok(tokens)
+    }
+
+    // 一文字分だけ読み進めてTokenを返す
+    fn next_return_token(&mut self, token: Token) -> Option<Token> {
+        self.chars.next();
+        Some(token)
+    }
+
+    // 先頭の文字からマッチしたトークンを返す
+    fn next_token(&mut self) -> Result<Option<Token>, LexerError> {
+        match self.chars.peek() {
+            Some(c) => match c {
+                // ' 'もしくは'\n'はWhiteSpace
+                c if c.is_whitespace() || *c == '\n' => {
+                    Ok(self.next_return_token(Token::WhiteSpace))
+                }
+                // number: ([0-9]|'+'|'-'|'.')[0-9]*
+                c if c.is_numeric() || matches!(c, '+' | '-' | '.') => self.parse_number_token(),
+                // ident: [a-zA-Z][a-zA-Z0-9]*
+                c if c.is_alphabetic() => self.parse_ident_token(),
+                '#' => self.parse_comment_token(),
+                _ => Err(LexerError::new(&format!("error: {}", c))),
+            },
+            None => Ok(None),
+        }
+    }
+
+    fn parse_number_token(&mut self) -> Result<Option<Token>, LexerError> {
+        let mut number_str = String::new();
+        while let Some(&c) = self.chars.peek() {
+            // 1e10, 1E10, 1.000など
+            if c.is_numeric() | matches!(c, '+' | '-' | 'e' | 'E' | '.') {
+                self.chars.next();
+                number_str.push(c);
+            } else {
+                break;
+            }
+        }
+        match number_str.parse::<f64>() {
+            Ok(number) => Ok(Some(Token::Number(number))),
+            Err(e) => Err(LexerError::new(&format!("error: {}", e))),
+        }
+    }
+
+    fn parse_ident_token(&mut self) -> Result<Option<Token>, LexerError> {
+        let mut ident_str = String::new();
+        while let Some(&c) = self.chars.peek() {
+            if c.is_alphanumeric() {
+                self.chars.next();
+                ident_str.push(c);
+            } else {
+                break;
+            }
+        }
+        match &*ident_str {
+            "def" => Ok(Some(Token::Def)),
+            "extern" => Ok(Some(Token::Extern)),
+            _ => Ok(Some(Token::Ident(ident_str))),
+        }
+    }
+
+    fn parse_comment_token(&mut self) -> Result<Option<Token>, LexerError> {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! tokenize {
+        ($input:expr) => {
+            Lexer::new($input).tokenize().unwrap()
+        };
+    }
+
+    #[test]
+    fn test_number() {
+        // integer
+        let tokens = tokenize!("1234567890");
+        assert_eq!(tokens[0], Token::Number(1234567890f64));
+        let tokens = tokenize!("+123");
+        assert_eq!(tokens[0], Token::Number(123f64));
+        // float
+        let tokens = tokenize!("-0.001");
+        assert_eq!(tokens[0], Token::Number(-0.001));
+        let tokens = tokenize!(".001");
+        assert_eq!(tokens[0], Token::Number(0.001));
+        // exponent
+        let tokens = tokenize!("1e-10");
+        assert_eq!(tokens[0], Token::Number(0.0000000001));
+        let tokens = tokenize!("+2E10");
+        assert_eq!(tokens[0], Token::Number(20000000000f64))
+    }
+
+    #[test]
+    fn test_ident() {
+        // keyword
+        let tokens = tokenize!("def");
+        assert_eq!(tokens[0], Token::Def);
+        let tokens = tokenize!("extern");
+        assert_eq!(tokens[0], Token::Extern);
+        let tokens = tokenize!("rust");
+        assert_eq!(tokens[0], Token::Ident("rust".to_owned()))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod lexer;
+
+use crate::lexer::Lexer;
 use std::io::{self, Write};
 
 fn main() {
@@ -13,126 +16,8 @@ fn main() {
         if input.as_str() == ".quit\n" {
             break;
         }
-        let tokens = gettok(input.as_str());
+        let tokens = Lexer::new(&input).tokenize().unwrap();
         println!("{:?}", tokens);
         continue;
-    }
-}
-
-#[derive(Debug, PartialEq)]
-enum Token {
-    Eof,
-    // commands
-    Def,
-    Extern,
-    // primary
-    Ident(String),
-    Number(f64),
-    Ope(String),
-}
-
-// lexer
-fn gettok(input: &str) -> Vec<Token> {
-    let mut tokens: Vec<Token> = vec![];
-    let mut input_itr = input.chars();
-    let mut next_input = || -> Option<char> { input_itr.next() };
-    'main: loop {
-        if let Some(c) = next_input() {
-            if c.is_whitespace() {
-                continue;
-            }
-            // ident: [a-zA-Z][a-zA-Z0-9]*
-            if c.is_alphabetic() {
-                let mut ident_chars: Vec<char> = vec![c];
-                'ident_loop: loop {
-                    if let Some(next_c) = next_input() {
-                        if next_c.is_alphanumeric() {
-                            ident_chars.push(next_c);
-                            continue 'ident_loop;
-                        } else {
-                            let ident_str: String = ident_chars.into_iter().collect();
-                            match &*ident_str {
-                                "def" => tokens.push(Token::Def),
-                                "extern" => tokens.push(Token::Extern),
-                                _ => tokens.push(Token::Ident(ident_str)),
-                            }
-                            continue 'main;
-                        }
-                    } else {
-                        break 'main;
-                    }
-                }
-            }
-            if c.is_ascii_digit() || c == '.' {
-                let mut num_chars: Vec<char> = vec![c];
-                'num_loop: loop {
-                    if let Some(next_c) = next_input() {
-                        if next_c.is_ascii_digit() || next_c == '.' {
-                            num_chars.push(next_c);
-                            continue 'num_loop;
-                        } else {
-                            let num_str: String = num_chars.into_iter().collect();
-                            if let Ok(num_val) = num_str.parse::<f64>() {
-                                tokens.push(Token::Number(num_val))
-                            };
-                            continue 'main;
-                        }
-                    } else {
-                        break 'main;
-                    }
-                }
-            }
-            if c == '#' {
-                // Comment until end of line
-                loop {
-                    if let Some(next_c) = next_input() {
-                        if next_c != '\n' && next_c != '\r' {
-                            continue;
-                        } else {
-                            continue 'main;
-                        }
-                    } else {
-                        break 'main;
-                    }
-                }
-            }
-            // Otherwise, just return the character as its ascii value
-            tokens.push(Token::Ope(c.into()));
-        } else {
-            break;
-        }
-    }
-    tokens
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{gettok, Token};
-
-    #[test]
-    fn lexer_works() {
-        assert_eq!(gettok("def\n"), vec![Token::Def]);
-        assert_eq!(gettok("extern\n"), vec![Token::Extern]);
-        assert_eq!(gettok("hello\n"), vec![Token::Ident("hello".to_string())]);
-        assert_eq!(gettok("1.23\n"), vec![Token::Number(1.23)]);
-        let text_with_comment = "
-def hello # this is comment
-# this line is comment
-extern def hello
-";
-        assert_eq!(
-            gettok(text_with_comment),
-            vec![
-                Token::Def,
-                Token::Ident("hello".to_string()),
-                Token::Extern,
-                Token::Def,
-                Token::Ident("hello".to_string())
-            ]
-        );
-        assert_eq!(
-            gettok("1 + 3\n"),
-            vec![Token::Number(1.), Token::Ope("+".into()), Token::Number(3.)]
-        )
     }
 }
